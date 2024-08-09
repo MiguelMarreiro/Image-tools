5785b32e9d46a729c6963fee73625feeae911310
MiguelMarreiro <miguel.marreiro>
2024-08-09 15:54:36 +0100

    add gui file selector, add RGBA to RGB conversion

--- convert2jpg.py
+++ convert2jpg.py
@@ -1,13 +1,31 @@
 from PIL import Image, ImageDraw, ImageFilter
+from tkinter import filedialog
 
+# Open the image file
+file_path = filedialog.askopenfilename()
 
-# Open the WebP image file
-webp_image_name = "The-God-Machine-Blind-Guardian2"
-im = Image.open(f"images/{webp_image_name}.webp")
+# Print the selected file path
+if file_path:
+    print(f"Selected file: {file_path}")
+else:
+    print("No file selected.")
 
-# Display the image
-im.show()
+image_format = file_path.split(".")[1]
+image_name = file_path.split("/")[-1].split(".")[0]
+
+im = Image.open(file_path)
+
+# Convert RGBA to RGB
+if im.mode == 'RGBA':
+    # Create a new background (white) image
+    background = Image.new("RGB", im.size, (255, 255, 255))
+    # Paste the original image on the background
+    background.paste(im, mask=im.split()[3])  # 3 is the alpha channel
+    im = background
+
+# # Display the image
+# im.show()
 
 # Convert and save the image to JPEG format
-jpeg_image_path = f"output/{webp_image_name}.jpeg"
+jpeg_image_path = fr"output/{image_name}.jpeg"
 im.save(jpeg_image_path, 'JPEG', quality=90)
